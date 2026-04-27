ae839714722c4da92dd9551598b4cec81816948f
tharun-2125 <tharunnatarajan2125@gmail.com>
2026-04-27 08:49:31 +0530

    Update studentmate_web.py

--- studentmate_web.py
+++ studentmate_web.py
@@ -3,10 +3,13 @@ from huggingface_hub import InferenceClient
 from pptx import Presentation
 import datetime, json, hashlib, os
 
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 MODEL = "deepseek-ai/DeepSeek-V3-0324"
 
-client = InferenceClient(api_key=HF_TOKEN, provider="auto")
+client = InferenceClient(
+    api_key=st.secrets["HF_TOKEN"],
+    provider="auto"
+)
 
 st.set_page_config(page_title="StudentMate AI", page_icon="🤖", layout="wide")
 
